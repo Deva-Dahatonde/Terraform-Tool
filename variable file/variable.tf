cff36267b46bd66cd94a50862e3639ea17258738
Devanand Dahatonde <devadahatonde2001@gmail.com>
2026-02-14 20:54:14 +0530

    Create variable.tf

---
+++ variable file/variable.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+  default     = "demo-instance"
+}
